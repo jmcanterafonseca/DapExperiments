9eadc5c2eb0cb652154bba968c8101e6ba416a01
Jose M. Cantera <jmcf@tid.es>
2011-10-17 13:14:16 +0200

    Removing stuff

--- src/com/immersion/android/ImmVibe.java
+++
@@ -1,6 +0,0 @@
-package com.immersion.android;
-
-public class ImmVibe {
-	public native int OpenDevice(int number,String s);
-	public native void setDevicePropertyInt32(int handle, int number, int value);
-}
